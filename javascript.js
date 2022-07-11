bf0ba97dfb9782a3664a5d1fe6914024dcf94ab6
Rishabh-441 <90997234+Rishabh-441@users.noreply.github.com>
2022-07-11 19:09:41 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+var Name = "Rishabh Tiwari";
+var roll = "201550115";
+var date = new Date().toLocaleDateString();
+var clg = "GLA University, Mathura";
+
+
+document.getElementById('name').innerHTML = Name;
+document.getElementById('roll').innerHTML = roll;
+document.getElementById('clg').innerHTML = clg;
+document.getElementById('date').innerHTML = date;
